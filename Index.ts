de4f549c387b3e7323e244cf8efb1767dfe05e5b
Heitor de Sousa Miranda <heitordesousa@gmail.com>
2018-12-27 07:53:21 -0200

    redis

--- Index.ts
+++ Index.ts
@@ -13,12 +13,43 @@ import express from "express";
 import { RouteBundle } from "./lib/Router";
 import { json } from "body-parser";
 import { HandlerMiddleware } from "./middlewares/HandlerMiddleware";
+import { print, createClient } from "redis";
+
+const client = createClient({
+  port: 18973,
+  host: "redis-18973.c8.us-east-1-4.ec2.cloud.redislabs.com",
+  password: "C41AXTFwgtWW4p2M6Y9ubQNg5KHGP7Pd",
+  db: 0
+});
+
+client.on("error", x => {
+  console.log("Error " + x);
+});
+
+client.on("connect", x => {
+  console.log("connected");
+  client.set("htht", new Date().toString(), print);
+});
+
+// client.set("ssid", "ade31fe5af", x => {
+//   console.log(x);
+// });
 
 const app: express.Application = express();
 
 app.use(json());
 
 app.use(HandlerMiddleware.definition);
+
+app.use("/param", (req, res) => {
+  res.send(process.env.TEST || "didn't work");
+});
+
+app.use("/redis", (req, res) => {
+  client.get("htht", (e, r) => {
+    res.send(r);
+  });
+});
 
 RouteBundle.register(app);
 
--- index.js
+++ index.js
@@ -15,8 +15,33 @@ var express_1 = __importDefault(require("express"));
 var Router_1 = require("./lib/Router");
 var body_parser_1 = require("body-parser");
 var HandlerMiddleware_1 = require("./middlewares/HandlerMiddleware");
+var redis_1 = require("redis");
+var client = redis_1.createClient({
+    port: 18973,
+    host: "redis-18973.c8.us-east-1-4.ec2.cloud.redislabs.com",
+    password: "C41AXTFwgtWW4p2M6Y9ubQNg5KHGP7Pd",
+    db: 0
+});
+client.on("error", function (x) {
+    console.log("Error " + x);
+});
+client.on("connect", function (x) {
+    console.log("connected");
+    client.set("htht", new Date().toString(), redis_1.print);
+});
+// client.set("ssid", "ade31fe5af", x => {
+//   console.log(x);
+// });
 var app = express_1.default();
 app.use(body_parser_1.json());
 app.use(HandlerMiddleware_1.HandlerMiddleware.definition);
+app.use("/param", function (req, res) {
+    res.send(process.env.TEST || "didn't work");
+});
+app.use("/redis", function (req, res) {
+    client.get("htht", function (e, r) {
+        res.send(r);
+    });
+});
 Router_1.RouteBundle.register(app);
 app.listen(process.env.PORT || 3500);
