f75e84d465124bd3625088dc931341687b918fc9
heeitoor <heitordesousa@gmail.com>
2019-02-12 13:47:03 -0200

    Update FrekvensRouter.js

--- routes/FrekvensRouter.js
+++ routes/FrekvensRouter.js
@@ -1,52 +1,82 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const Enums_1 = require("../core/Enums");
-const Global_1 = require("../Global");
-const mongodb_1 = require("mongodb");
-class FrekvensRoute {
-}
-FrekvensRoute.prefix = "/frekvens";
-FrekvensRoute.definition = express_1.Router()
-    .get(FrekvensRoute.prefix + "/class/:teacherId", (req, res) => __awaiter(this, void 0, void 0, function* () {
-    res.send(yield Global_1._mediator.send({
-        body: {
-            teacherId: req.params.teacherId
-        },
-        type: Enums_1.Handler.FrekvensGetClassesRequest
-    }));
-}))
-    .get(FrekvensRoute.prefix + "/student", (req, res) => __awaiter(this, void 0, void 0, function* () { }))
-    .post(FrekvensRoute.prefix + "/user", (req, res) => __awaiter(this, void 0, void 0, function* () {
-    const mongoClient = new mongodb_1.MongoClient("mongodb://heroku_hz7348t9:mc8oaj44u60kadnfssvtmdst3c@ds113749.mlab.com:13749/heroku_hz7348t9", { useNewUrlParser: true });
-    const d = yield mongoClient.connect();
-    const db = d.db();
-    const r = yield db.collection("user").findOne({ userName: req.body.userName });
-    if (!r) {
-        res.send({
-            error: true
-        });
-        return;
+const express = require('express');
+var connection = require('./db/connection');
+
+class Routers {
+    attendance() {
+        var attendanceRouter = express.Router();
+        
+        attendanceRouter
+          .get('/:teacherId', async (req, res) => {})
+          .post('/', async (req, res) => {
+            const client = await connection.instance();
+            
+            const result = await client.query('insert into attendance (date,"teacherClassId",status) values ($1, $2, $3);', [
+              req.body.date,
+              req.body.teacherId,
+              'OPEN'
+            ]);
+            
+            res.sendStatus(200);
+          })
+          .put('/', async (req, res) => {});
+        
+        return attendanceRouter;
     }
-    res.send(r);
-}))
-    .post(FrekvensRoute.prefix + "/attendance", (req, res) => __awaiter(this, void 0, void 0, function* () {
-    res.send(yield Global_1._mediator.send({
-        body: {
-            id: req.body.id,
-            date: req.body.date,
-            teacherId: req.body.teacherId,
-            attendances: req.body.attendances
-        },
-        type: Enums_1.Handler.FrekvensSaveAttendanceRequest
-    }));
-}));
-exports.FrekvensRoute = FrekvensRoute;
+    
+    class() {
+        var classRouter = express.Router();
+        
+        classRouter
+          .get('/', async (req, res) => {})
+          .post('/', async (req, res) => {})
+          .put('/', async (req, res) => {});
+        
+        return classRouter;
+    }
+    
+    teacherClass() {        
+        var teacherClassRouter = express.Router();
+        
+        teacherClassRouter
+          .get('/:teacherId/:date', async (req, res) => {
+            const client = await connection.instance();
+
+            const result = await client.query(
+              `
+              select c.id "classId", c.name "className", a.id "attendanceId", a.date "attendanceDate" from "teacherClass" tc join "class" c on c.id = tc."classId" left join attendance a on tc.id = a."teacherClassId" and a.date = '${
+                req.params.date
+              }' where tc."teacherId" = ${req.params.teacherId};`
+            );
+
+            res.send(result.rows);
+          })
+          .post('/', async (req, res) => {})
+          .put('/', async (req, res) => {});
+        
+        return teacherClassRouter;
+    }
+    
+    teacher() {
+        var teacherRouter = express.Router();
+        
+        teacherRouter
+          .get('/', async (req, res) => {
+            const client = await connection.instance();
+            const result = await client.query(`select * from teacher`);
+            res.send(result.rows);
+          })
+          .post('/', async (req, res) => {
+            const client = await connection.instance();
+            const result = await client.query(`
+            select * from teacher t
+            where t."userName" = '${req.body.userName}' and t."password" = '${req.body.password}'
+            `);
+            res.send(result.rows);
+          });
+        
+        return teacherRouter;
+    }
+}
+
+
+
